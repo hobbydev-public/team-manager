25f6b47ec551f2727b28620fd35906b97756ad85
HobbyDev <hobbydev.public@gmail.com>
2017-08-07 13:09:35 +0300

    API response format changed

--- src/main/webapp/WEB-INF/res/app/js/es6/components/topNavBar/controller.js
+++ src/main/webapp/WEB-INF/res/app/js/es6/components/topNavBar/controller.js
@@ -1,9 +1,18 @@
 export default class TopNavBarController {
-    constructor(/*$uibModal*/) {
+    constructor($scope,
+                /*$uibModal,*/
+                principalService) {
         'ngInject';
 
-        //this.modalProvider = $uibModal;
+        $scope.ctrl = this; // after this assignment, controller instance is available in template either by 'ctrl' or by alias, defined in 'controllerAs'
+        let _ctrl = this;
+        let rootScope = $scope.$root;
 
+        //this.modalProvider = $uibModal;
+        principalService.getCurrentUser(function (currentUser) {
+            _ctrl.principal = rootScope.appContext.currentUser;
+            _ctrl._initData();
+        });
         this._initData();
 
     }

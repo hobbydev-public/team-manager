b0d99917cc1b01f2b75bd7da5214c892fe445482
HobbyDev <hobbydev.public@gmail.com>
2017-08-10 17:18:11 +0300

    moved editUserDataForm component to companies module

--- src/main/webapp/WEB-INF/res/app/js/es6/modules/profile/index.js
+++ src/main/webapp/WEB-INF/res/app/js/es6/modules/profile/index.js
@@ -1,9 +1,12 @@
 /* Modules */
 import angular from 'angular';
 
+/* Components */
+import components from './components';
+
 /* Config */
 import routing from './routing';
 
-export default angular.module('app.profile', [])
+export default angular.module('app.profile', [components])
     .config(routing)
     .name;
